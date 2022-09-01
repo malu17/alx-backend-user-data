9d5aa5634227ea8a7d6bdc0b0f79bbb87d357b08
Lesm <88344019+malu17@users.noreply.github.com>
2022-09-01 20:02:11 +0300

    add all tasks of personal_data Directory.

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
-"""
-Encrypting passwords
-"""
+""" Encrypting passwords """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ Returns a salted, hashed password, which is a byte string """
-    encoded = password.encode()
-    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
-
-    return hashed
+    """ expects one string argument name password and returns a salted,
+        hashed password, which is a byte string. """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Validates the provided password matches the hashed password """
-    valid = False
-    encoded = password.encode()
-    if bcrypt.checkpw(encoded, hashed_password):
-        valid = True
-    return valid
+    """ expects 2 arguments and returns a boolean. """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
